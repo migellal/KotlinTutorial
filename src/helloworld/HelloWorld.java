b6f5699c41d69c87c06b4beaa8596fecee7bf448
migellal <michalgellert@gmail.com>
2016-09-19 06:59:00 +0200

    HelloWorld

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+/**
+ * Created by micha on 19.09.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
---
+++ src/helloworld/hello.kt
@@ -0,0 +1,9 @@
+package helloworld
+
+/**
+ * Created by micha on 19.09.2016.
+ */
+
+fun main(args: Array<String>) {
+    println("hello, world!")
+}
